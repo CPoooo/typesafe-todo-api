94c407a83222d5aa6fcbc79af7dbaffd0843ef79
cpool <cpool@kbsi.com>
2026-06-25 09:51:47 -0500

    super necessary ascii art added on app.listen()

--- src/server.js
+++ src/server.js
@@ -1,4 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var bcrypt_1 = require("bcrypt");
@@ -6,94 +42,6 @@ var db_1 = require("./db/db");
 var drizzle_orm_1 = require("drizzle-orm"); // when we (me) make the src/queries dir we can remove all this
 var schema_1 = require("./db/schema");
 var jsonwebtoken_1 = require("jsonwebtoken");
-var app = (0, express_1.default)();
-var PORT = process.env.PORT || 3000;
-// 1. you logged in good job 
-// 2. hey your password is wrong <- only so many attempts, and there is something with not 
-// saying something in the response about the password for security but I can not remember 
-// 3. your email does not exist or is typo
-// SO THE ANSWER TO THIS: always return a generic 401 with invalid credentials, an attacker should not be able to brute
-// force emails and see what emails are or are not registered
-// POST   /auth/login          returns a JWT
-// can fully type the Request with ts and zod after working
-app.post('/auth/login', function (req, res) {
-    // login, check with db and 
-    // logic then return jwt. go look into more depth on jwt btw
-    // something about rotating them or something <- custom solution before libary for now
-    // how does bcrypt interact with jwt
-    // from my understanding this moment: user logs in with password and it gets hashed via bcrypt usually with 10 salting rounds
-    // then if login succeeds attach the jwt to this user
-    // so the next time they log in they will already have one? so would we need to check the password again 
-    // or is jwt used on all other routes except this one 
-});
-// POST   /auth/register       create account
-app.post('/auth/register', function (req, res) {
-    // make account and give jwt
-    // only if the user does not exist already
-    // can move this query into src/queries later. would probably also have a handlers and routes dir as well
-    // {email: string, password: string}
-    // check if we already have this email -> what code to reject with?
-    var _a = req.body, email = _a.email, password = _a.password, name = _a.name;
-    try {
-        var result = yield db_1.db.selectDistinct().from(schema_1.usersTable).where((0, drizzle_orm_1.eq)(schema_1.usersTable.email, email));
-        if (result.length > 0) {
-            // user with this email exists, normally best practice here when registering
-            // is to force the user to verify this email exists. and do verification that way.
-            // for learning purposes I will just return email already exists for now -> which leads to account enumeration (BAD)
-            res.status(400).send(JSON.stringify({ error: 'email already exists so go brute force it and take their lunch money' }));
-        }
-        var password_hash = yield bcrypt_1.default.hash(password, 10);
-        var new_user = yield db_1.db.insert(schema_1.usersTable).values({ email: email, name: name, password: password_hash }).returning();
-        // now take new user and return them a jwt
-        var user = { userId: new_user[0].id };
-        var token = jsonwebtoken_1.default.sign(user, process.env.SECRET_KEY); // find a good way to error earlier in this file with this doesnt exist
-        // maybe some zod on the .env like I saw the Syntax guy do (for now we assert dominance, I mean assert not null)
-        res.status(201).send(JSON.stringify(token));
-    }
-    catch (error) {
-        console.log("Something went wrong querying the db for user by email");
-        // should make a discriminated union for all http errors and some custom
-        res.status(500).send(JSON.stringify({ error: "Internal Server Error" }));
-    }
-});
-// POST   /auth/logout         invalidate token
-app.post('/auth/signout', function (req, res) {
-    // get deeper explanation on what invalidate token means here 
-});
-// ==============================================
-// would make a separate router per entity in larger app (at least i like the routes and handler dir pattern I use)
-// routes is an Express Router and each handler is a function that gets the db connection and makes a request
-// look into how to make sure the pool is efficient in typescript (I imagine the defaul pooling option I think that drizzle sets up with postgres is fine)
-// GET    /todos               your todos only
-app.get('/todos', function (req, res) {
-});
-// GET    /todos/:id           must be yours
-app.get('/todos/:id', function (req, res) {
-});
-// POST   /todos               create
-app.post('/todos', function (req, res) {
-});
-// PATCH  /todos/:id           update title or completed
-app.patch('/todos/:id', function (req, res) {
-});
-// DELETE /todos/:id           delete
-app.delete('/todos/:id', function (req, res) {
-});
-// // stretch
-// GET    /todos?completed=true      filter
-// GET    /todos?sort=createdAt      sort
-app.get('/todos', function (req, res) {
-});
-app.get('/todos', function (req, res) {
-});
-app.listen(PORT, function () {
-    console.log("App listening on port: ".concat(PORT));
-});
-function parseEmail(s) {
-    if (!s.includes('@'))
-        throw new Error('invalid email');
-    return s;
-}
 // SPEC FOR CHALLENGE: TODO APP WITH AUTH
 // POST   /auth/register       create account
 // POST   /auth/login          returns a JWT
@@ -103,6 +51,129 @@ function parseEmail(s) {
 // POST   /todos               create
 // PATCH  /todos/:id           update title or completed
 // DELETE /todos/:id           delete
+// GET    /todos?completed=true      filter
+// GET    /todos?sort=createdAt      sort
+var app = (0, express_1.default)();
+var PORT = process.env.PORT || 3000;
+app.post('/auth/login', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log("logging in user");
+        return [2 /*return*/];
+    });
+}); });
+app.post('/auth/register', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, email, password, name, result, password_hash, new_user, user, token, error_1;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = req.body, email = _a.email, password = _a.password, name = _a.name;
+                _b.label = 1;
+            case 1:
+                _b.trys.push([1, 5, , 6]);
+                return [4 /*yield*/, db_1.db.selectDistinct().from(schema_1.usersTable).where((0, drizzle_orm_1.eq)(schema_1.usersTable.email, email))];
+            case 2:
+                result = _b.sent();
+                if (result.length > 0) {
+                    res.status(400).send(JSON.stringify({ error: 'email already exists so go brute force it and take their lunch money' }));
+                }
+                return [4 /*yield*/, bcrypt_1.default.hash(password, 10)];
+            case 3:
+                password_hash = _b.sent();
+                return [4 /*yield*/, db_1.db.insert(schema_1.usersTable).values({ email: email, name: name, password: password_hash }).returning()];
+            case 4:
+                new_user = _b.sent();
+                user = { userId: new_user[0].id };
+                token = jsonwebtoken_1.default.sign(user, process.env.SECRET_KEY) // find a good way to error earlier in this file with this doesnt exist
+                ;
+                res.status(201).send(JSON.stringify(token));
+                return [3 /*break*/, 6];
+            case 5:
+                error_1 = _b.sent();
+                console.log("Something went wrong querying the db for user by email");
+                // should make a discriminated union for all http errors and some custom
+                res.status(500).send(JSON.stringify({ error: "Internal Server Error" }));
+                return [3 /*break*/, 6];
+            case 6: return [2 /*return*/];
+        }
+    });
+}); });
+// POST   /auth/logout         invalidate token
+app.post('/auth/signout', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        // get deeper explanation on what invalidate token means here 
+        console.log("siging out user and invalidating jwt token for said user");
+        return [2 /*return*/];
+    });
+}); });
+// ==============================================
+// would make a separate router per entity in larger app (at least i like the routes and handler dir pattern I use)
+// routes is an Express Router and each handler is a function that gets the db connection and makes a request
+// look into how to make sure the pool is efficient in typescript (I imagine the defaul pooling option I think that drizzle sets up with postgres is fine)
+// GET    /todos               your todos only
+app.get('/todos', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log('todo');
+        return [2 /*return*/];
+    });
+}); });
+// GET    /todos/:id           must be yours
+app.get('/todos/:id', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log('todo');
+        return [2 /*return*/];
+    });
+}); });
+// POST   /todos               create
+app.post('/todos/:id', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log('todo');
+        return [2 /*return*/];
+    });
+}); });
+// PATCH  /todos/:id           update title or completed
+app.patch('/todos/:id', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var id;
+    return __generator(this, function (_a) {
+        id = req.body.id;
+        console.log("patching todo ".concat(id));
+        res.send("todo/patch/:id");
+        return [2 /*return*/];
+    });
+}); });
+// DELETE /todos/:id           delete
+app.delete('/todos/:id', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var id;
+    return __generator(this, function (_a) {
+        id = req.body.id;
+        console.log("deleting todo ".concat(id));
+        res.send("todo/delete/:id");
+        return [2 /*return*/];
+    });
+}); });
 // // stretch
 // GET    /todos?completed=true      filter
 // GET    /todos?sort=createdAt      sort
+app.get('/todos', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log('TODO');
+        return [2 /*return*/];
+    });
+}); });
+app.get('/todos', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        console.log('TODO');
+        return [2 /*return*/];
+    });
+}); });
+app.listen(PORT, function () {
+    console.log("App listening on port: ".concat(PORT));
+});
+/* three base64 encoded chunks
+ JWT: header.payload.signature jwt process in my words
+ user sends a payload to login like {email:cameron@gmail.com, password: isthissentoverthewireunhashedbtwwellwehavetlsnevermindlol?}
+ the server will say okay they logged in successfully i trust you, I am now going to take your payload
+ and hash it with my secret key <- do i need to know how this works fully? might as well know a lil
+ server then sends back the jwt: header.payload.signature <- sig is the thing we just hashed using secret key and their payload
+ then next time when they make a request they send along the jwt and we take
+ header.payload portion and check against our secret key (it will only match if the payload is not tampered with)reject or continue (user authenticated)
+*/ 
--- src/server.ts
+++ src/server.ts
@@ -4,152 +4,7 @@ import bcrypt from "bcrypt"
 import { db } from './db/db';
 import { eq } from 'drizzle-orm'; // when we (me) make the src/queries dir we can remove all this
 import { InsertUser, usersTable } from './db/schema';
-import jsonwebtoken from 'jsonwebtoken'
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-
-// three base64 encoded chunks 
-// JWT: header.payload.signature
-
-// jwt process in my words
-// user sends a payload to login like {email:cameron@gmail.com, password: isthissentoverthewireunhashedbtwwellwehavetlsnevermindlol?}
-// the server will say okay they logged in successfully i trust you, I am now going to take your payload
-// and hash it with my secret key <- do i need to know how this works fully? might as well know a lil
-// server then sends back the jwt: header.payload.signature <- sig is the thing we just hashed using secret key and their payload
-// then next time when they make a request they send along the jwt and we take 
-// header.payload portion and check against our secret key (it will only match if the payload is not tampered with)
-// reject or continue (user authenticated) 
-
-// what goes into a token when we sign it? 
-type UnverifiedRequest = { email: string, password: string } // z.email here later 
-// type JWTPayload = {header: string, payload: UnverifiedRequest, signature: string} // should this be user specific? i mean they cant login to the todo table
-type JWTPayload = { userId: number } // only the pk from the db, min information needed to verify who you are
-
-type VerifiedPayload = UnverifiedRequest & { __brand: 'verified', iat: 'thisisaguessidkifthesearedatesornumbers', exp: 1000 }
-type AuthenticatedRequest = UnverifiedRequest & { __brand: 'authenticated' }
-
-// can i make this a type to validate the request coming in? 
-// or just use zod?
-// PARSE DONT VALIDATE <- good philosophy and what zod does as well and rust too
-// you want to parse into a strongly typed thing instead of just validating the unknown
-
-// type Email = string 
-// // something with @ and . <- how to type this well 
-// CHECK BOTTOM TO SEE THE BRANDED TYPES AND PROBABLY JUST USE ZOD HERE
-
-// turn this into a discriminated union
-// go read more on algebraic data types <- tie them into typescript and nextjs specifically
-type LoginRequest = { email: Email, password: string }
-
-type StatusCode = 200 | 201 | 404 // etc
-type LoginResponse = { status: StatusCode } // some enum or something. this could be in three states
-// 1. you logged in good job 
-// 2. hey your password is wrong <- only so many attempts, and there is something with not 
-// saying something in the response about the password for security but I can not remember 
-
-// 3. your email does not exist or is typo
-// SO THE ANSWER TO THIS: always return a generic 401 with invalid credentials, an attacker should not be able to brute
-// force emails and see what emails are or are not registered
-
-// POST   /auth/login          returns a JWT
-// can fully type the Request with ts and zod after working
-app.post('/auth/login', (req: , res: Response) => {
-    // login, check with db and 
-    // logic then return jwt. go look into more depth on jwt btw
-    // something about rotating them or something <- custom solution before libary for now
-    // how does bcrypt interact with jwt
-    // from my understanding this moment: user logs in with password and it gets hashed via bcrypt usually with 10 salting rounds
-    // then if login succeeds attach the jwt to this user
-    // so the next time they log in they will already have one? so would we need to check the password again 
-    // or is jwt used on all other routes except this one 
-})
-
-// POST   /auth/register       create account
-app.post('/auth/register', (req, res) => {
-    // make account and give jwt
-    // only if the user does not exist already
-    // can move this query into src/queries later. would probably also have a handlers and routes dir as well
-    // {email: string, password: string}
-    // check if we already have this email -> what code to reject with?
-    const { email, password, name } = req.body
-
-    try {
-        const result = await db.selectDistinct().from(usersTable).where(eq(usersTable.email, email  ))
-
-        if (result.length > 0) {
-            // user with this email exists, normally best practice here when registering
-            // is to force the user to verify this email exists. and do verification that way.
-            // for learning purposes I will just return email already exists for now -> which leads to account enumeration (BAD)
-            res.status(400).send(JSON.stringify({ error: 'email already exists so go brute force it and take their lunch money' }))
-        }
-
-        const password_hash = await bcrypt.hash(password, 10)
-        const new_user = await db.insert(usersTable).values({ email, name, password: password_hash }).returning()
-        // now take new user and return them a jwt
-        const user = { userId: new_user[0].id };
-
-        const token = jsonwebtoken.sign(user, process.env.SECRET_KEY!) // find a good way to error earlier in this file with this doesnt exist
-        // maybe some zod on the .env like I saw the Syntax guy do (for now we assert dominance, I mean assert not null)
-
-        res.status(201).send(JSON.stringify(token))
-    } catch (error) {
-        console.log("Something went wrong querying the db for user by email")
-        // should make a discriminated union for all http errors and some custom
-        res.status(500).send(JSON.stringify({ error: "Internal Server Error" }))
-    }
-})
-
-// POST   /auth/logout         invalidate token
-app.post('/auth/signout', (req, res) => {
-    // get deeper explanation on what invalidate token means here 
-})
-
-// ==============================================
-// would make a separate router per entity in larger app (at least i like the routes and handler dir pattern I use)
-// routes is an Express Router and each handler is a function that gets the db connection and makes a request
-// look into how to make sure the pool is efficient in typescript (I imagine the defaul pooling option I think that drizzle sets up with postgres is fine)
-
-// GET    /todos               your todos only
-app.get('/todos', (req: , res: Response) => {
-})
-
-// GET    /todos/:id           must be yours
-app.get('/todos/:id', (req: , res: Response) => {
-})
-
-// POST   /todos               create
-app.post('/todos', (req: , res: Response) => {
-})
-
-// PATCH  /todos/:id           update title or completed
-app.patch('/todos/:id', (req: , res: Response) => {
-})
-// DELETE /todos/:id           delete
-app.delete('/todos/:id', (req: , res: Response) => {
-})
-
-// // stretch
-// GET    /todos?completed=true      filter
-// GET    /todos?sort=createdAt      sort
-app.get('/todos', (req: , res: Response) => {
-})
-
-app.get('/todos', (req: , res: Response) => {
-})
-
-app.listen(PORT, () => {
-    console.log(`App listening on port: ${PORT}`);
-})
-
-// ==================================================
-type Email = string & { __brand: 'Email' }
-
-function parseEmail(s: string): Email {
-    if (!s.includes('@')) throw new Error('invalid email');
-    return s as Email;
-}
+import jwt from 'jsonwebtoken'
 
 // SPEC FOR CHALLENGE: TODO APP WITH AUTH
 
@@ -163,6 +18,123 @@ function parseEmail(s: string): Email {
 // PATCH  /todos/:id           update title or completed
 // DELETE /todos/:id           delete
 
+// GET    /todos?completed=true      filter
+// GET    /todos?sort=createdAt      sort
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.post('/auth/login', async (req, res) => {
+    console.log("logging in user")
+    res.send('hello from: auth/login')
+})
+
+app.post('/auth/register', async (req, res) => {
+    const { email, password, name } = req.body
+
+    try {
+        const result = await db.selectDistinct().from(usersTable).where(eq(usersTable.email, email))
+
+        if (result.length > 0) {
+            res.status(400).send(JSON.stringify({ error: 'email already exists so go brute force it and take their lunch money' }))
+        }
+
+        const password_hash = await bcrypt.hash(password, 10)
+        const new_user = await db.insert(usersTable).values({ email, name, password: password_hash }).returning()
+        const user = { userId: new_user[0].id };
+
+        const token = jwt.sign(user, process.env.SECRET_KEY!) // find a good way to error earlier in this file with this doesnt exist
+
+        res.status(201).send(JSON.stringify(token))
+    } catch (error) {
+        console.log("Something went wrong querying the db for user by email")
+        // should make a discriminated union for all http errors and some custom
+        res.status(500).send(JSON.stringify({ error: "Internal Server Error" }))
+    }
+})
+
+// POST   /auth/logout         invalidate token
+app.post('/auth/signout', async (req, res) => {
+    // get deeper explanation on what invalidate token means here 
+    console.log("siging out user and invalidating jwt token for said user")
+    res.send("hello from /auth/signout")
+})
+
+// ==============================================
+// would make a separate router per entity in larger app (at least i like the routes and handler dir pattern I use)
+// routes is an Express Router and each handler is a function that gets the db connection and makes a request
+// look into how to make sure the pool is efficient in typescript (I imagine the defaul pooling option I think that drizzle sets up with postgres is fine)
+
+// GET    /todos               your todos only
+app.get('/todos', async (req, res) => {
+    console.log('todo')
+    res.send("hello from /todos")
+})
+
+// GET    /todos/:id           must be yours
+app.get('/todos/:id', async (req, res) => {
+    console.log('todo')
+    res.send("hello from /todos/:id")
+})
+
+// POST   /todos               create
+app.post('/todos/:id', async (req, res) => {
+    console.log('todo')
+    res.send("hello from POST /todos/:id")
+})
+
+// PATCH  /todos/:id           update title or completed
+app.patch('/todos/:id', async (req, res) => {
+    const { id } = req.body
+    console.log(`patching todo ${id}`)
+    res.send("hello from PATCH /todos/:id")
+})
+// DELETE /todos/:id           delete
+app.delete('/todos/:id', async (req, res) => {
+    const { id } = req.body
+    console.log(`deleting todo ${id}`)
+    res.send("hello from /todos/:id")
+})
+
 // // stretch
 // GET    /todos?completed=true      filter
 // GET    /todos?sort=createdAt      sort
+app.get('/todos', async (req, res) => {
+    console.log('TODO')
+    res.send("hello from /todos?completed=true") // filter only completed
+})
+
+app.get('/todos', async (req, res) => {
+    console.log('TODO')
+    res.send("hello from /sort?createdAt") // sorted by createdAt Desc (most recent i think is desc? lol)
+})
+
+app.listen(PORT, () => {
+    console.log(`
+\x1b[36m
+ ████████╗ ██████╗ ██████╗  ██████╗      █████╗ ██████╗ ██╗
+    ██╔══╝██╔═══██╗██╔══██╗██╔═══██╗    ██╔══██╗██╔══██╗██║
+    ██║   ██║   ██║██║  ██║██║   ██║    ███████║██████╔╝██║
+    ██║   ██║   ██║██║  ██║██║   ██║    ██╔══██║██╔═══╝ ██║
+    ██║   ╚██████╔╝██████╔╝╚██████╔╝    ██║  ██║██║     ██║
+    ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝     ╚═╝  ╚═╝╚═╝     ╚═╝
+\x1b[0m
+\x1b[33m  ⚡ Server blazing on port ${PORT}\x1b[0m
+\x1b[90m  ────────────────────────────────\x1b[0m
+\x1b[32m  ✓ Database connected
+  ✓ Routes mounted
+  ✓ JWT auth ready\x1b[0m
+\x1b[90m  ────────────────────────────────\x1b[0m
+    `)
+})
+
+/* three base64 encoded chunks 
+ JWT: header.payload.signature jwt process in my words
+ user sends a payload to login like {email:cameron@gmail.com, password: isthissentoverthewireunhashedbtwwellwehavetlsnevermindlol?}
+ the server will say okay they logged in successfully i trust you, I am now going to take your payload
+ and hash it with my secret key <- do i need to know how this works fully? might as well know a lil
+ server then sends back the jwt: header.payload.signature <- sig is the thing we just hashed using secret key and their payload
+ then next time when they make a request they send along the jwt and we take 
+ header.payload portion and check against our secret key (it will only match if the payload is not tampered with)reject or continue (user authenticated)
+*/
